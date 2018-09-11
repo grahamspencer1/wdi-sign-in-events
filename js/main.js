1fc8e1a21557042e514e2ffabb095c59818be861
Graham Spencer <grahamspence@gmail.com>
2018-09-11 16:01:17 -0400

    added logon modal

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  var logOn = document.querySelector('.signin');
+  var displayModal = document.querySelector('.modal');
+  var hideModal = document.querySelector('.close')
+
+  logOn.addEventListener('click', function() {
+    displayModal.style.display = 'block';
+  });
+
+  hideModal.addEventListener('click', function() {
+    hideModal.style.display = 'none';
+  });
+
+});
